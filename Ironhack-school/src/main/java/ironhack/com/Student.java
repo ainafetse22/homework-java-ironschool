f7dd2c035346ef66ad61181630ddd34be60b6e9a
Darren <111098562+DJH23@users.noreply.github.com>
2024-03-14 20:28:50 +0100

    Add unfound teacher Id check

--- Ironhack-school/src/main/java/ironhack/com/Student.java
+++ Ironhack-school/src/main/java/ironhack/com/Student.java
@@ -1,6 +1,8 @@
 package ironhack.com;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -13,21 +15,37 @@ public class Student extends Person {
     public Student(String name, String address, String email) {
         super(name);
         this.address = address;
-        this.email = email;
+        this.setEmail(email);
+        courseList = new ArrayList<>();
     }
 
     public Student(String name, String address, String email, List<Course> courseList) {
         super(name);
         this.address = address;
-        this.email = email;
+        this.setEmail(email);
         this.courseList = courseList;
     }
 
+    public void setEmail(String email) {
+        if (!Utils.ValidateEmail.isValidEmail(email)) {
+            throw new IllegalArgumentException("Invalid email format");
+        }
+        this.email = email;
+    }
+
     public void printInfo() {
-        System.out.println("Id: " + getId()
+        System.out.print("Id: " + getId()
                 + ", Name: " + getName()
                 + ", Address: " + getAddress()
                 + ", Email: " + getEmail()
-                + ", Courses: " + (getCourseList() != null ? getCourseList().toString() : "N/A"));
+                + ", Courses: [");
+                for(Course course:getCourseList()){
+                    System.out.print(course.getName()+" ");
+                };
+                System.out.println("]");
+    }
+
+    public void enrollInCourse(Course course) {
+        this.courseList.add(course);
     }
 }
